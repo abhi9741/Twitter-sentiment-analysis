4939c9a1ad35b47c412c92ffc94bc66e48d37dae
Abhinav Reddy Nimma <abhinavreddynimma@gmail.com>
2017-08-24 11:01:19 +0530

    adding algorithm for getting places avilable for trends and getting trends from a particular place

--- twitter_analysis.py
+++ twitter_analysis.py
@@ -10,6 +10,30 @@ ats='YVrScxouBU6aa1q57SSxfnxRG70D6oqNlZFHK0v6iykbY'    #ats stands for acess tok
 
 auth=ah(ck,cs)     #auth stands for authorisation
 auth.set_access_token(at,ats)
-
 api=t.API(auth)
+
+#for finding the countries/places available for finding trends
+trends = api.trends_available()
+for trend in trends:
+    print trend
+
+# for finding trends at a current place
+trends_at_a_place=api.trends_place(2295414)
+
+
+# trends_at_a_place is a list with only one element in it, which is a dict  
+trendsp=trends_at_a_place
+data = trendsp[0]
+
+# get  the trends
+trends = data['trends']
+
+#get the name from each trend
+names = [trend['name'] for trend in trends]
+
+# put all the names together with a ' ' separating them
+trendsName = ' '.join(names)
+print(trendsName)
+
+
 
